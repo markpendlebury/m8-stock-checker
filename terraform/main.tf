ab1674a263c2f0152a98525efb6c65bc68d63f67
Mark Pendlebury <mark.pendlebury@elesoft.dev>
2024-05-15 21:54:46 +0100

    updating gitignore and re-enabling lambda

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,22 @@
-# resource "aws_lambda_function" "lambda" {
-#   function_name    = "${var.service_name}-lambda"
-#   image_uri        = "${aws_ecr_repository.ecr.repository_url}:latest"
-#   package_type     = "Image"
-#   timeout          = var.timeout
-#   memory_size      = var.memory_size
-#   source_code_hash = null
-#   role             = aws_iam_role.lambda_exec.arn
+resource "aws_lambda_function" "lambda" {
+  function_name    = "${var.service_name}-lambda"
+  image_uri        = "${aws_ecr_repository.ecr.repository_url}:latest"
+  package_type     = "Image"
+  timeout          = var.timeout
+  memory_size      = var.memory_size
+  source_code_hash = null
+  role             = aws_iam_role.lambda_exec.arn
 
 
-#   environment {
-#     variables = {
-#       user_key  = var.user_key,
-#       api_token = var.api_token
-#     }
-#   }
+  environment {
+    variables = {
+      user_key  = var.user_key,
+      api_token = var.api_token
+    }
+  }
 
-#   depends_on = [
-#     aws_ecr_repository.ecr
-#   ]
-# }
+  depends_on = [
+    aws_ecr_repository.ecr
+  ]
+}
 

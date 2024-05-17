dbdbfa7c3ddef030e9a68ac2a5735b229237444c
Mark Pendlebury <mark.pendlebury@elesoft.dev>
2024-05-17 20:45:32 +0100

    testing lower case variable string

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -7,6 +7,12 @@ def lambda_handler(event, context):
         url = "https://dirtywave.com/products/m8-tracker-model-02"
         html = get_html(url)
 
+        teststring = "TEST_STRING"
+        log_line("lambda_handler", "DEBUG", teststring=teststring)
+        
+        lower_case_test_string = teststring.lower()
+        log_line("lambda_handler", "DEBUG", lower_case_test_string=lower_case_test_string)
+
         if event.get("test").lower() == "true":
             if in_stock(html):
                 send_push_notification("In stock")
